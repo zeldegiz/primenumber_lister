7893ad7ee90579a909c5f548a3c1358026831649
zeldegiz <77746113+zeldegiz@users.noreply.github.com>
2021-03-25 23:28:25 +0400

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,17 @@ def primechecker(x):
 y=int(input("Enter : "))
 t=time.time()
 r=3
-
+h = [2]
 while(r<y):
   if(primechecker(r)==1):
-    print(r)
+    h.append(r)
+  if(primechecker(r+2)==1):
+    h.append(r+2)
   if(primechecker(r+4)==1):
-    print(r+4)
+    h.append(r+4)
   if(primechecker(r+6)==1):
-    print(r+6)
+    h.append(r+6)
   if(primechecker(r+8)==1):
-    print(r+8)
+    h.append(r+8)
   r=r+10
 print(time.time()-t)
